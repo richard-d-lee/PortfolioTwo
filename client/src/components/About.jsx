f387a3a56a0978f1258d6b2f9195a69ec8eb0ee0
Richard Lee <richdelee@gmail.com>
2021-03-16 11:03:50 -0600

    framework for about page added

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -3,6 +3,16 @@ import React, { useState } from 'react';
 function About(props) {
   return (
     <div className="about">
+      <div className="about-left">
+        <div className="carousel">
+
+        </div>
+        <div>
+
+        </div>
+      </div>
+      <div className="bio">
+      </div>
       <center>
         <label>Hello, My Name is Buz</label>
       </center>
