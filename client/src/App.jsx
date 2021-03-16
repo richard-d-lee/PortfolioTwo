caea1b1f5835f14fef6c40e145f6205d280d34dd
Richard Lee <richdelee@gmail.com>
2021-03-16 16:03:22 -0600

    More style perfectionism, 1px bug found >_>

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -24,7 +24,7 @@ class App extends React.Component {
 
   render() {
     return (
-      <div>
+      <div id='full-app'>
         <Banner return={this.returnHome} />
         <Landing
           about={this.aboutHandler}
