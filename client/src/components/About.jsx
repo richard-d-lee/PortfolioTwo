709de83bb88e9d4e5cd8b7cdb12fd459477a3c06
Richard Lee <richdelee@gmail.com>
2021-03-16 12:11:07 -0600

    Carousel complete

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -1,21 +1,60 @@
 import React, { useState } from 'react';
+import Carousel from 'react-bootstrap/Carousel'
+import imageOne from './images/MEnJB.jpg'
+import imageTwo from './images/MEnJS.jpg'
+import imageThree from './images/Food.jpg'
 
 function About(props) {
   return (
     <div className="about">
       <div className="about-left">
-        <div className="carousel">
+        <center>
+          <div className="carousel">
+            <Carousel fade>
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src={imageOne}
+                  alt="First slide"
+                />
+                <Carousel.Caption>
+                  <h3>Me With My Dog</h3>
+                  <p>Junebug is fiercely loyal and incredibly loving. I'm lucky to have such a tough mutt at my side.</p>
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src={imageTwo}
+                  alt="Second slide"
+                />
 
-        </div>
-        <div>
+                <Carousel.Caption>
+                  <h3>My Girlfriend Jesse</h3>
+                  <p>Jesse has provided major love and support in my life and I couldn't accomplish the things I do without her at my side.</p>
+                </Carousel.Caption>
+              </Carousel.Item>
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src={imageThree}
+                  alt="Third slide"
+                />
+
+                <Carousel.Caption>
+                  <h3>Steamboat Springs</h3>
+                  <p>Jesse and I love to travel. Here is a photo she took eating at Taco Cabo in Steamboat Springs.</p>
+                </Carousel.Caption>
+              </Carousel.Item>
+            </Carousel>
+          </div>
+        </center>
+        <div className="sites">
 
         </div>
       </div>
       <div className="bio">
       </div>
-      <center>
-        <label>Hello, My Name is Buz</label>
-      </center>
     </div>
   )
 }
--- client/src/components/MyCard.jsx
+++ client/src/components/MyCard.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
-import gif from './giphy.gif'
+import gif from './images/giphy.gif'
 
 function MyCard(props) {
   return (
