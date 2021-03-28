11be0c30f1549847a9591a5814fabe18bdf7d31f
Richard Lee <richdelee@gmail.com>
2021-03-28 12:24:49 -0600

    Changes to Artwork Page

--- client/src/components/Artwork.jsx
+++ client/src/components/Artwork.jsx
@@ -1,10 +1,22 @@
 import React, { useState } from 'react';
+import Card from 'react-bootstrap/Card';
+import gif from './images/giphy.gif';
+import Button from 'react-bootstrap/Button';
 
 function Artwork(props) {
   return (
     <div className="artwork">
-        <label>Here are a few of my paintings!</label>
-    </ div>
+      <Card className="my-card" style={{ width: '11.88%', backgroundColor: "black", borderRadius: '15px', border: "1px solid white" }}>
+        <Card.Img variant="top" className="card-image" src={gif} style={{ width: '14rem', height: 'auto', borderRadius: '15px' }}>
+        </Card.Img>
+        <Button
+          type="button"
+          variant="light"
+          className="artwork-button">
+          Card
+        </Button>
+      </Card>
+    </div>
   )
 }
 
