443407958cc16d27ef72a755876fe4596402b760
Richard Lee <richdelee@gmail.com>
2021-03-28 12:43:17 -0600

    Finished artwork splash page formatting

--- client/src/components/Artwork.jsx
+++ client/src/components/Artwork.jsx
@@ -6,16 +6,50 @@ import Button from 'react-bootstrap/Button';
 function Artwork(props) {
   return (
     <div className="artwork">
-      <Card className="my-card" style={{ width: '11.88%', backgroundColor: "black", borderRadius: '15px', border: "1px solid white" }}>
-        <Card.Img variant="top" className="card-image" src={gif} style={{ width: '14rem', height: 'auto', borderRadius: '15px' }}>
-        </Card.Img>
-        <Button
-          type="button"
-          variant="light"
-          className="artwork-button">
-          Card
+      <div id="top-two">
+        <Card className="art-card" style={{ backgroundColor: "black", borderRadius: '15px', border: "1px solid white" }}>
+          <Card.Img variant="top" className="card-image" src={gif} style={{ width: '14rem', height: 'auto', borderRadius: '15px' }}>
+          </Card.Img>
+          <Button
+            type="button"
+            variant="light"
+            className="artwork-button">
+            Acrylic
         </Button>
-      </Card>
+        </Card>
+        <Card className="art-card" style={{ backgroundColor: "black", borderRadius: '15px', border: "1px solid white" }}>
+          <Card.Img variant="top" className="card-image" src={gif} style={{ width: '14rem', height: 'auto', borderRadius: '15px' }}>
+          </Card.Img>
+          <Button
+            type="button"
+            variant="light"
+            className="artwork-button">
+            Digital
+          </Button>
+        </Card>
+      </div>
+      <div id="bottom-two">
+        <Card className="art-card" style={{ backgroundColor: "black", borderRadius: '15px', border: "1px solid white" }}>
+          <Card.Img variant="top" className="card-image" src={gif} style={{ width: '14rem', height: 'auto', borderRadius: '15px' }}>
+          </Card.Img>
+          <Button
+            type="button"
+            variant="light"
+            className="artwork-button">
+            Writing
+          </Button>
+        </Card>
+        <Card className="art-card" style={{ backgroundColor: "black", borderRadius: '15px', border: "1px solid white" }}>
+          <Card.Img variant="top" className="card-image" src={gif} style={{ width: '14rem', height: 'auto', borderRadius: '15px' }}>
+          </Card.Img>
+          <Button
+            type="button"
+            variant="light"
+            className="artwork-button">
+            Other
+          </Button>
+        </Card>
+      </div>
     </div>
   )
 }
