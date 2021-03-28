c7bc0a264131540a9fac0879669d65aaa144773b
Richard Lee <richdelee@gmail.com>
2021-03-28 10:05:18 -0600

    Styling for Project page complete

--- client/src/components/Project.jsx
+++ client/src/components/Project.jsx
@@ -1,20 +1,25 @@
 import React, { useState } from 'react';
 import gif from './images/giphy.gif';
+import Button from 'react-bootstrap/Button';
 
 function Project(props) {
-  return (
-    <div className="projects">
-        <div className="project">
-            <center>
-                <label id="project-title">My Project</label>
-            </center>
-            <img src={gif}></img>
-            <center>
-                <label id="project-description">These are my projects!</label>
-            </center>
-        </div>
-    </ div>
-  )
+    return (
+        <div>
+            <div className="project">
+                <div id="project-title">
+                    <p id="project-title-text">
+                        <center>
+                            Title
+                    </center>
+                    </p>
+                </div>
+                <img id="project-gif" src={gif}></img>
+                <center>
+                    <label id="project-description">These are my projects!</label>
+                </center>
+            </div>
+        </ div>
+    )
 }
 
 export default Project;
