c4da2471536c5f16f7140767d177e461957877bd
Richard Lee <richdelee@gmail.com>
2021-03-29 18:11:00 -0600

    Added acrylics

---
+++ client/src/components/Acrylics.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+
+function Acrylic(props) {
+    return (
+      <div className="acrylic">
+        <p>These are my acrylic projects!</p>
+      </div>
+    );
+  
+}
+
+export default Acrylic;
