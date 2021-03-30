cbcfb94cba50ca465c89bb65acf1bf289dcf7477
Richard Lee <richdelee@gmail.com>
2021-03-29 18:10:17 -0600

    Two projects added

---
+++ client/src/components/Digital.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+
+function Digital(props) {
+    return (
+      <div className="digital">
+        <p>These are my digital projects!</p>
+      </div>
+    );
+  
+}
+
+export default Digital;
---
+++ client/src/components/Writing.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+
+function Writing(props) {
+    return (
+      <div className="writing">
+        <p>These are my writing projects!</p>
+      </div>
+    );
+  
+}
+
+export default Writing;
