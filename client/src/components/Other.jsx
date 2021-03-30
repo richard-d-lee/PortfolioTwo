aba1a69f03f66b9e4581a580422a3444dd6feff1
Richard Lee <richdelee@gmail.com>
2021-03-29 18:11:32 -0600

    Others added

---
+++ client/src/components/Other.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+
+function Other(props) {
+    return (
+      <div className="other">
+        <p>These are my other projects!</p>
+      </div>
+    );
+  
+}
+
+export default Other;
